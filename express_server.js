c6c79c58f717b0952ad54b1cd0d56003e973c62c
Zio <zi.ao.tan@outlook.com>
2021-05-10 20:57:33 +0000

    Add hello_world ejs file and index.ejs file

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -18,18 +23,10 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+  const templateVars = { greeting: 'Hello, World'};
+  res.render('hello_world', templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
